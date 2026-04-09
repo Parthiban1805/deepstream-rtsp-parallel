808856de195f543e71783f99402bf96dfefa87f4
Parthiban <parthi.s1805@gmail.com>
2026-04-09 10:51:31 +0530

    feat: add application core configuration

---
+++ parallel_rtsp/config.py
@@ -0,0 +1,17 @@
+# config.py
+
+PGIE_PEOPLENET_CONFIG_FILE = "config_infer_primary_peoplenet.txt"
+PGIE_TRAFFICCAMNET_CONFIG_FILE = "config_infer_primary_trafficcamnet.txt"
+
+MAIN_MUX_CONFIG_FILE = "config_main_mux.txt"
+
+
+TRACKER_CONFIG_FILE = "dsnvanalytics_tracker_config.txt"
+
+TILED_OUTPUT_WIDTH = 1920
+TILED_OUTPUT_HEIGHT = 1080
+
+GST_CAPS_FEATURES_NVMM = "memory:NVMM"
+
+OSD_PROCESS_MODE = 0
+OSD_DISPLAY_TEXT = 1
